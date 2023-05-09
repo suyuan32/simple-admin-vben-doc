06325bf19040b3188bf89a98fe70e8f2f2218ff5
Ryan SU <yuansu.china.work@gmail.com>
2023-05-09 08:17:37 +0800

    docs: update deploy

--- src/.vuepress/theme.ts
+++ src/.vuepress/theme.ts
@@ -3,7 +3,7 @@ import { enNavbar, zhNavbar } from "./navbar/index.js";
 import { enSidebar, zhSidebar } from "./sidebar/index.js";
 
 export default hopeTheme({
-  hostname: "https://doc.ryansu.pro",
+  hostname: "https://vben.ryansu.pro",
 
   author: {
     name: "Ryan SU",
