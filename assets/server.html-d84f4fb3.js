a37f3e9afac92ed9e69f91a60e902a42499a2a0b
suyuan32 <1137661202@qq.com>
2023-05-10 08:28:23 +0000

    Deploying to gh-pages from @ suyuan32/simple-admin-vben-doc@ffc0969e43b3efdff4d4e2da8117a096ca970905 🚀

---
+++ assets/server.html-d84f4fb3.js
@@ -0,0 +1,10 @@
+import{_ as e,X as s,Y as n,a2 as a}from"./framework-13746272.js";const i={},t=a(`<h1 id="test-server" tabindex="-1"><a class="header-anchor" href="#test-server" aria-hidden="true">#</a> test server</h1><p>There is a simple Node.js test background interface service in the project <code>/test/server</code>, implemented with Koa2</p><h2 id="use" tabindex="-1"><a class="header-anchor" href="#use" aria-hidden="true">#</a> use</h2><div class="language-bash line-numbers-mode" data-ext="sh"><pre class="language-bash"><code>
+<span class="token builtin class-name">cd</span> ./test/server
+
+<span class="token comment"># install dependencies</span>
+<span class="token function">pnpm</span>
+
+<span class="token comment"># run the service</span>
+<span class="token function">pnpm</span> start
+
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div><p>After the service runs successfully, you can access the test upload interface and websocket interface service</p>`,5),r=[t];function c(d,l){return s(),n("div",null,r)}const v=e(i,[["render",c],["__file","server.html.vue"]]);export{v as default};
